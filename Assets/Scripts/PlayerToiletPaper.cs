0c85854044b81b8ecfcd385a2bf7681dd44b0ead
TheRealKraken <lachlan.jmv@gmail.com>
2022-10-25 11:51:42 +1100

    Fixed ToiletPaperProjectile
    The projectile now works as intended. The only problem is that the projectile interacts with the player's collider. Thus disabling the player from being able to shoot downwards.

--- Assets/Scripts/PlayerToiletPaper.cs
+++ Assets/Scripts/PlayerToiletPaper.cs
@@ -6,10 +6,12 @@ public class PlayerToiletPaper : MonoBehaviour
 {
     public GameObject toiletPaper;
     public float toiletPaperSpeed;
+    private Camera mainCam;
+    private Vector3 mousePos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCam = gameObject.transform.GetChild(0).gameObject.GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -17,9 +19,10 @@ public class PlayerToiletPaper : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 mousePos = Input.mousePosition;
+            mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 rotation = mousePos - transform.position;
             GameObject thrownToiletPaper = Instantiate<GameObject>(toiletPaper, gameObject.transform);
-            thrownToiletPaper.GetComponent<Rigidbody2D>().velocity = new Vector2(mousePos.x * toiletPaperSpeed, mousePos.y * toiletPaperSpeed);
+            thrownToiletPaper.GetComponent<Rigidbody2D>().velocity = rotation.normalized * toiletPaperSpeed;
         }
     }
 }
