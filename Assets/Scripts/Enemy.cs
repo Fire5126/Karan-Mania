2bf7bb85d503a9ff5f11206ce0f908532377b9c3
TheRealKraken <lachlan.jmv@gmail.com>
2022-12-28 16:09:59 +1100

    added Reef Studios Branding and logo to game properties

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,23 @@ public class Enemy : MonoBehaviour
     public float MaxHealth = 10;
     GameObject player;
     public int damage;
+    bool ableToAttack = false;
+    float attackDelay = 1;
+    float nextAttack;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+
+        if (ableToAttack == true && Time.time > nextAttack)
+        {
+            ApplyPlayerDamage();
+            nextAttack = Time.time + attackDelay;
+        }
     }
 
     public void UpdateHealth(float damage)
@@ -28,7 +40,15 @@ public class Enemy : MonoBehaviour
     {
         if (collision.transform.tag == "Player")
         {
-            ApplyPlayerDamage();
+            ableToAttack = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            ableToAttack = false;
         }
     }
 
