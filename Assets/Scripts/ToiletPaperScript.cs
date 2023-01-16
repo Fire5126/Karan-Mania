e3f1184e18d552f415f9395d6a110fb5b71a7fa0
TheRealKraken <lachlan.jmv@gmail.com>
2023-01-17 09:48:40 +1100

    added slight fix to projectile spawning in colliders

--- Assets/Scripts/ToiletPaperScript.cs
+++ Assets/Scripts/ToiletPaperScript.cs
@@ -7,15 +7,29 @@ public class ToiletPaperScript : MonoBehaviour
     PlayerController player;
     PlayerAttack attackScript;
     Rigidbody2D rb;
+
+    bool collideCheck = true;
+    public Collider2D toiletPaperCollider;
+
     // Start is called before the first frame update
     void Start()
     {
+        Invoke("StopCollideCheck", 0.1f);
         rb = GetComponent<Rigidbody2D>();
 
         attackScript = FindObjectOfType<PlayerAttack>();
         player = FindObjectOfType<PlayerController>();
         rb.AddForce(transform.right * attackScript.projectileSpeed, ForceMode2D.Impulse);
+        if(rb.velocity.magnitude < attackScript.projectileSpeed)
+        {
+            DestroyToiletPaper();
+        }
         Invoke("DestroyToiletPaper", attackScript.toiletPaperDuration);
+    }
+
+    void StopCollideCheck()
+    {
+        collideCheck = false;
     }
 
     // Update is called once per frame
@@ -29,17 +43,29 @@ public class ToiletPaperScript : MonoBehaviour
         if (collision.gameObject.tag == "Enemy")
         {
             attackScript.EnemyHit(collision);
+            DestroyToiletPaper();
         }
-        DestroyToiletPaper();
+        
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collideCheck)
+        {
+            Physics2D.IgnoreLayerCollision(6, 7, true);
+            return;
+        }
         if (collision.gameObject.tag != "Projectile")
         {
             DestroyToiletPaper();
         }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Physics2D.IgnoreLayerCollision(6, 7, false);
     }
 
     void DestroyToiletPaper()
